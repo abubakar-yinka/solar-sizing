2ab430c7be4d8939e7d3d980062f26a43ae3b14d
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-24 04:44:55 +0100

    chore: configure babel appropriately

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".ts", ".tsx", ".js"],
+        },
+      ],
+      "react-native-reanimated/plugin",
+    ],
   };
 };
