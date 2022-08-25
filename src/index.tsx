2af372a770b85a323a2f14bcb1d980d174d6fb5b
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-25 10:37:50 +0100

    feat: linked redux store to react app

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { StatusBar } from "react-native";
 // Styles
 import { ThemeProvider } from "styled-components/native";
 import theme from "./styles/theme/index";
 // Navigation
 import Navigation from "./app/routes";
+// Store
+import { configureAppStore } from "store/configureStore";
 
 interface Props {}
 
+const store = configureAppStore();
+
 const App: React.FC<Props> = () => (
-  <ThemeProvider theme={theme}>
-    <StatusBar
-      backgroundColor={theme.colors.constants.background}
-      barStyle="light-content"
-    />
-    <Navigation />
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        backgroundColor={theme.colors.constants.background}
+        barStyle="light-content"
+      />
+      <Navigation />
+    </ThemeProvider>
+  </Provider>
 );
 
 export default App;
