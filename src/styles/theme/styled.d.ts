d7df0113667de3eafb7139a986beb35fdbe6671b
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2022-08-25 10:41:05 +0100

    chore: added theme classes to style types

--- src/styles/theme/styled.d.ts
+++ src/styles/theme/styled.d.ts
@@ -1,6 +1,10 @@
 import "styled-components";
 import { ThemeType } from "./index";
 
+export type ThemeClasses = {
+  color: "theme-dark" | "theme-light";
+};
+
 declare module "styled-components" {
   export interface DefaultTheme extends ThemeType {}
 }
