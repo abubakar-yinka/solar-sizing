63d0bbb2b7ba5a6fd2ba69d91113abdc8fe60fa2
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-04-06 01:01:08 +0100

    wip: home screen implementation

--- src/app/screens/Home/index.tsx
+++ src/app/screens/Home/index.tsx
@@ -1,16 +1,23 @@
-import { Text, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import React from "react";
 import Container from "app/components/Container";
 // Locales
 import i18n from "locales/index";
+// Layout
+import MainView from "app/layout/MainView";
+// Components
+import H1 from "app/components/Typography/H1";
 
 const Home: React.FC = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Container>
-        <Text>{i18n.t("title")}</Text>
-      </Container>
-    </SafeAreaView>
+    <MainView
+      headerShown={true}
+      title={`Home`}
+      backIconPresent={true}
+      isLight={false}
+    >
+      <H1 textColor="#000">{i18n.t("title")}</H1>
+    </MainView>
   );
 };
 
