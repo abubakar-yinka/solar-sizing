3268afa30e4e7bea8fb9bffa87eb70ce21294206
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-04-12 19:48:35 +0100

    chore: added required navigation types

--- src/app/routes/types.ts
+++ src/app/routes/types.ts
@@ -1,10 +1,24 @@
 import { StackNavigationProp } from "@react-navigation/stack";
 
 export type RootStackParamList = {
+  Login: undefined;
   Home: undefined;
+  Recommendation: undefined;
 };
+
+export type NavigationProps = StackNavigationProp<RootStackParamList>;
+
+export type LoginScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "Login"
+>;
 
 export type HomeScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   "Home"
+>;
+
+export type RecommendationScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  "Recommendation"
 >;
