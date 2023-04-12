fb98c5a6692037a711978648ba44de8e12b1a723
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-04-12 20:17:42 +0100

    chore: defined the data model for the app state

---
+++ src/app/screens/Home/slice/types.ts
@@ -0,0 +1,31 @@
+export interface AppState {
+  firstName: string;
+  lastName: string;
+  totalPowerConsumption: string;
+  sunHours: string;
+  savedDate: Date;
+  batteryAutonomyDays: string;
+  appliances: Appliance[];
+  noOfAppliances: number;
+  totalWattHoursPerDay: number;
+  recommendations: Recommendation[];
+  loading: boolean;
+  error: string;
+}
+
+export interface Appliance {
+  appliance: { name: string; powerRating: number };
+  hoursOnPerDay: number;
+  wattHoursPerDay: number;
+}
+
+export interface Recommendation {
+  name: number;
+  models: {
+    amp: number;
+    volt: number;
+    model: string;
+    count: number;
+  }[];
+  panel: number;
+}
