627184d671634d5705417e4b112febd3b3bd435f
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-04-12 20:29:42 +0100

    chore: modified app theme

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,8 +1,21 @@
 // TODO: Put all your theme colors with images here.
 const lightTheme = {
-  title: "black",
-  description: "gray",
-  background: "white",
+  primary: "#3B46F1", // Also Active Color
+  pressedPrimary: "rgba(29, 29, 147, .8)", // Also Active Color
+  disabled: "#E4E3E8",
+  disabledBtnText: "#948EA5",
+  searchInput: "rgba(239, 240, 246, 0.56)",
+  searchPlaceholderText: "#989AAA",
+  otpInput: "#F5F5F5",
+  lightGreyText: "#4D4D4D", // Also Input placeholder text color
+  inputBorder: "#ECEBED",
+  greyText: "#555555",
+  darkGreyText: "#333333",
+  whiteText: "#FFFFFF",
+  blackText: "#000000",
+  campaignText: "#00C2AB",
+  beneficiaryText: "#323232",
+  inactiveTabColor: "#5C5C5C",
 };
 
 const darkTheme = {
@@ -17,6 +30,7 @@ const constantTheme = {
   secondary: "#EDEDED",
   textGrey: "#333333",
   textLightGrey: "#4D4D4D",
+  recommendationText: "#070D5F",
 };
 
 const Theme = {
