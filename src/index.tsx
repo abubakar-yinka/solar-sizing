e751e0b38d13afe183dece8bf653a2e122985e00
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-04-12 20:22:14 +0100

    chore: implemented store subscription to keep the app state in sync with the local storage of the device and persisted data

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { StatusBar } from "react-native";
+import Toast from "react-native-toast-message";
 // Styles
 import { ThemeProvider } from "styled-components/native";
 import theme from "./styles/theme/index";
@@ -8,21 +9,58 @@ import theme from "./styles/theme/index";
 import Navigation from "./app/routes";
 // Store
 import { configureAppStore } from "store/configureStore";
+import { saveAppState } from "utils/storage";
+import { objectsEqual } from "utils/helpers";
 
-interface Props {}
+interface Props {
+  viewedOnboarding: boolean;
+}
 
 const store = configureAppStore();
 
-const App: React.FC<Props> = () => (
+store.subscribe(async () => {
+  const state = store.getState();
+  console.log("State subscribed!");
+
+  const isStateEqual = objectsEqual(state.app, {
+    firstName: "A",
+    lastName: "I",
+    totalPowerConsumption: "0.00",
+    sunHours: "0",
+    savedDate: new Date(),
+    batteryAutonomyDays: "0",
+    appliances: [
+      {
+        appliance: { name: "", powerRating: 0 },
+        hoursOnPerDay: 1,
+        wattHoursPerDay: 0,
+      },
+    ],
+    totalWattHoursPerDay: 0,
+    noOfAppliances: 0,
+    recommendations: [],
+    loading: false,
+    error: "",
+  });
+
+  if (!isStateEqual) {
+    console.log("State is not equal!");
+    await saveAppState(state.app);
+  } else {
+    console.log("State is equal!");
+  }
+});
+
+const App: React.FC<Props> = ({ viewedOnboarding }) => (
   <Provider store={store}>
     <ThemeProvider theme={theme}>
       <StatusBar
         backgroundColor={theme.colors.constants.background}
-        barStyle="light-content"
+        barStyle="dark-content"
       />
-      <Navigation />
+      <Navigation viewedOnboarding={viewedOnboarding} />
     </ThemeProvider>
+    <Toast />
   </Provider>
 );
-
 export default App;
