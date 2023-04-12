f911480b24dc42233e2fa984f44f5e0d19c09989
Abubakar Ibrahim <abubakaryinka75@gmail.com>
2023-04-12 20:26:49 +0100

    chore: added app state to root types

--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -1,5 +1,7 @@
+import { AppState } from "app/screens/Home/slice/types";
 import { ThemeState } from "styles/theme/slice/types";
 
 export interface RootState {
   theme?: ThemeState;
+  app: AppState;
 }
